bab9d4abd1882eb8e7876ce3b9e83c3b90607dbe
Manuel Salazar <manuelsalazar@unimayor.edu.co>
2026-04-20 19:35:27 -0500

    feat: factura y sus componentes

--- src/main/java/com/curso/spring/boot/springboot_di_factura/models/Item.java
+++ src/main/java/com/curso/spring/boot/springboot_di_factura/models/Item.java
@@ -1,5 +1,20 @@
 package com.curso.spring.boot.springboot_di_factura.models;
 
 public class Item {
+    private Product product;
+    private Integer quantity;
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 
+    
 }
--- src/main/java/com/curso/spring/boot/springboot_di_factura/models/Product.java
+++ src/main/java/com/curso/spring/boot/springboot_di_factura/models/Product.java
@@ -1,5 +1,23 @@
 package com.curso.spring.boot.springboot_di_factura.models;
 
 public class Product {
+    private String name;
+    private Integer price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 
 }
